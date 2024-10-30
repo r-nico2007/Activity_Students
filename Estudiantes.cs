4db903361f13f0724cf7858e590c3e8c0e801b53
Nicolas Ramirez <nicolasramz216@gmail.com>
2024-10-29 23:27:32 -0500

    Definimos propiedades

--- Estudiantes.cs
+++ Estudiantes.cs
@@ -8,5 +8,15 @@ namespace _10_
 {
     internal class Estudiantes
     {
+        public int Creditos { get; set; }
+        public int Estrato { get; set; }
+        public double ValorCredito { get; set; }
+
+        public Estudiantes(int creditos, int estrato, double valorCredito)
+        {
+            Creditos = creditos;
+            Estrato = estrato;
+            ValorCredito = valorCredito;
+        }
     }
 }
