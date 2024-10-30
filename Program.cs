2d1551a8c430ebc72de8d9359e434d6b4779436c
Nicolas Ramirez <nicolasramz216@gmail.com>
2024-10-30 00:13:36 -0500

    Le damos la orden al programa que solicite los datos, luego traemos la clase "Estudiantes" a la clase "Program"

--- Program.cs
+++ Program.cs
@@ -1,7 +1,27 @@
-﻿internal class Program
+﻿using _10_;
+
+public class Program
 {
     private static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+        while (true)
+        {
+            try
+            {
+                Console.WriteLine("Ingrese el número de créditos: ");
+                int créditos = int.Parse(Console.ReadLine());
+
+                Console.WriteLine("Ingrese el estrato: ");
+                int estrato = int.Parse(Console.ReadLine());
+
+                Console.WriteLine("Ingrese el valor por crédito: ");
+                double valorcredito = double.Parse(Console.ReadLine());
+
+                Estudiantes estudiantes = new Estudiantes(créditos, estrato, valorcredito);
+                estudiantes.MostrarInformacion();
+
+            }
+        }
     }
 }
+
